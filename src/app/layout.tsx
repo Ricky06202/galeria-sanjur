7cf8cfa201b35e170ebe958ddb844e778b1a472e
Ricardo A. Sanjur G. <91886986+Ricky06202@users.noreply.github.com>
2025-07-21 10:48:12 -0500

    Agregamos analytics

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,6 +1,7 @@
 import type { Metadata } from 'next'
 import './globals.css'
 import { SessionProvider } from 'next-auth/react'
+import { Analytics } from '@vercel/analytics/next'
 
 export const metadata: Metadata = {
   title: 'Medallas Sanjur',
@@ -21,6 +22,7 @@ export default function RootLayout({
         <SessionProvider>
           <div className="w-full max-w-7xl mx-auto">{children}</div>
         </SessionProvider>
+        <Analytics />
       </body>
     </html>
   )
